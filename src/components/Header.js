cba1e27ed234b2989db0b3306f909457cebd5471
Biswajit Sundara <biswajitsundara@gmail.com>
2023-10-20 23:34:08 +0530

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
 const Header = () => {
-    return ( <>
-    <img src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png" alt="" />
-    </>);
-}
- 
+  return (
+    <div className="absolute">
+      <img
+        className="w-56 px-8 py-2 bg-gradient-to-b from-black"
+        src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        alt=""
+      />
+    </div>
+  );
+};
+
 export default Header;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,15 @@
 import Header from "./Header";
 
 const Login = () => {
-    return ( <>
-    <Header />
-    </>);
-}
- 
+  return (
+    <>
+      <Header />
+      <img
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/a73c4363-1dcd-4719-b3b1-3725418fd91d/fe1147dd-78be-44aa-a0e5-2d2994305a13/IN-en-20231016-popsignuptwoweeks-perspective_alpha_website_small.jpg"
+        alt="bg"
+      />
+    </>
+  );
+};
+
 export default Login;
