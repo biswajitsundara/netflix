ef73bd4caa08e0cad162c61b69a1191879602a8b
Biswajit Sundara <biswajitsundara@gmail.com>
2023-11-06 22:46:53 +0530

    openai

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,7 +9,7 @@ const SearchBar = () => {
 
     return (
       <div className="pt-[10%] flex justify-center">
-        <form className="w-1/2 bg-black grid grid-cols-12">
+        <form className="w-1/2 bg-black grid grid-cols-12" onSubmit={(e)=> e.preventDefault()}>
           <input
             type="text"
             className="p-4 m-4 col-span-9"
